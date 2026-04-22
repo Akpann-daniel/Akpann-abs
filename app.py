1018c3709ebb147970659cd347993ab92ccb810c
Akpann-daniel <siret.akpan@gmail.com>
2026-04-22 11:30:32 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+# import the needed modules
+import pandas as pd
+import numpy as np
+from pandas import DataFrame
+import streamlit as st  
+import calendar
+print('i am here')
+# set up the page
+st.set_page_config(page_title="simple sales dashboard", page_icon="🐉👌🙌.")
+# title the page
+st.title("Simple Sales Dashboard App")
+# load the dataset
+data = pd.read_csv("Sample_data2.csv")
+st.write(data)
+
+# sort the months chronologically
+month_order = list(calendar.month_abbr)[1:7]
+data['Month_name']= pd.Categorical(data['Month_name'],categories=month_order,ordered=True)
+data=data.sort_values('Month_name')
+st.title('Monthly Data Overview')
+st.dataframe(data)
+
+# sidebar filter
+region_options = ['ALL'] + list(data['Region'].unique())
+month_options = ['ALL'] + month_order
+region = st.sidebar.selectbox('Select Region', region_options)
+month = st.sidebar.selectbox('Select Month', month_options)
+
+# filter the date
+data_filtered = data.copy()
+if region != 'ALL':
+    data_filtered = data_filtered[data_filtered['Region']==region]
+if month != 'ALL':
+    data_filtered = data_filtered[data_filtered['Month_name']==month]
+
+# KPIs
+total_sales = data_filtered['Sales'].sum()
+st.metric(label=f'Total Sales',value=f'${total_sales:,.2f}')
+
+# charts 1.
+st.write('### Sales by Product Category')
+chart1_data = data_filtered.groupby('Product_category')['Sales'].sum()
+st.bar_chart(chart1_data)
+
+# 2.
+st.write('### Sales Trend Over Time')
+chart2_data = data_filtered.groupby('Month_name')['Sales'].sum()
+st.line_chart(chart2_data)
